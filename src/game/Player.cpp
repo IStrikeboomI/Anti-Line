0ad02c90b6b1a4abb10fb5fb42dcdb24184b684a
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2022-06-06 21:57:38 -0500

    Got vertical drift (sort of) working

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -3,20 +3,43 @@
 Player::Player() : pos(0,0,SIDE_LENGTH, SIDE_LENGTH) {
 }
 
-void Player::moveUp(int distance) {
-	pos.setY(pos.getY() - distance);
+void Player::moveUp() {
+	yAxisAcceleration += .1;
+	yAxisVelocity -= yAxisAcceleration;
 }
 
-void Player::moveDown(int distance) {
-	pos.setY(pos.getY() + distance);
+void Player::moveDown() {
+	yAxisAcceleration += .1;
+	yAxisVelocity += yAxisAcceleration;
 }
 
-void Player::moveLeft(int distance) {
-	pos.setX(pos.getX() - distance);
+void Player::moveLeft() {
+	pos.setX(pos.getX() - 5);
 }
 
-void Player::moveRight(int distance) {
-	pos.setX(pos.getX() + distance);
+void Player::moveRight() {
+	pos.setX(pos.getX() + 5);
+}
+
+void Player::move() {
+
+}
+
+void Player::idle() {
+	if (yAxisAcceleration > MAX_ACCELERATION) {
+		yAxisAcceleration = MAX_ACCELERATION;
+	}
+	if (yAxisAcceleration > 0) {
+		yAxisAcceleration -= .1;
+	}
+	if (std::abs(yAxisVelocity) > MAX_VELOCITY) {
+		yAxisVelocity = std::signbit(yAxisVelocity) * MAX_VELOCITY;
+	}
+	if (yAxisVelocity > 0) {
+		yAxisVelocity -= std::signbit(yAxisVelocity) * .1;
+	}
+	pos.setY(pos.getY() + yAxisVelocity);
+	std::cout << yAxisAcceleration << "\n" << yAxisVelocity << "\n\n";
 }
 
 Gdiplus::Color Player::getColor() const {
--- src/game/Player.h
+++ src/game/Player.h
@@ -1,22 +1,29 @@
 #pragma once
+#include <cmath>
 #include "Pos.h"
 #include "../Paintable.h"
 #include "../Util.h"
+#include "../Direction.h"
 class Player : public Paintable{
 private:
 	//holds the alpha and rgb for the player color
 	//the way it works is by generating a random RGB value which is between 0 and 16777216 (FFFFFF in hex)
 	//then it uses "| 0xFF000000" to set the alpha of it to max because argb holds alpha first
 	int argb = Util::random(0, 0xFFFFFF) | 0xFF000000;
+	float xAxisAcceleration = 0, xAxisvelocity = 0, yAxisAcceleration = 0, yAxisVelocity = 0;
+	const float MAX_ACCELERATION = 5, MAX_VELOCITY = 10;
+	Direction direction = Direction::UP;
 public:
 	const static int SIDE_LENGTH = 30;
 	Pos pos;
 
 	Player();
-	void moveUp(int distance = 1);
-	void moveDown(int distance = 1);
-	void moveLeft(int distance = 1);
-	void moveRight(int distance = 1);
+	void moveUp();
+	void moveDown();
+	void moveLeft();
+	void moveRight();
+	void move();
+	void idle();
 	Gdiplus::Color getColor() const override;
 };
 
