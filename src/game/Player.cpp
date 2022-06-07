43ff5210638ffbe5a000e02725d109ffb6eb0fd0
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2022-06-07 12:10:52 -0500

    Added drifting movement for both axes

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -3,49 +3,96 @@
 Player::Player() : pos(0,0,SIDE_LENGTH, SIDE_LENGTH) {
 }
 
+//when moving up we need to lower the velocity
 void Player::moveUp() {
 	yAxisAcceleration += .2;
 	yAxisVelocity -= yAxisAcceleration;
 }
-
+//when moving down we need to increse the velocity
 void Player::moveDown() {
 	yAxisAcceleration += .2;
 	yAxisVelocity += yAxisAcceleration;
 }
-
+//when moving left we need to lower the velocity
 void Player::moveLeft() {
-	pos.setX(pos.getX() - 5);
+	xAxisAcceleration += .2;
+	xAxisVelocity -= xAxisAcceleration;
 }
-
+//when moving right we need to increse the velocity
 void Player::moveRight() {
-	pos.setX(pos.getX() + 5);
+	xAxisAcceleration += .2;
+	xAxisVelocity += xAxisAcceleration;
 }
 
-void Player::move() {
-
-}
-
+//called every tick
+//used for movement, both X and Y axis are the same thing, just different variables
 void Player::idle() {
+	//since acceleration gets added every time we move, we need to cap it
 	if (yAxisAcceleration > MAX_ACCELERATION) {
 		yAxisAcceleration = MAX_ACCELERATION;
 	}
+	//this lowers the acceleration so we don't keep moving
+	//>= 0.1 so we can actually lower the acceleration when we are supposed to
 	if (yAxisAcceleration >= 0.1) {
 		yAxisAcceleration -= .1;
+		//due to floating point errors we need to round down to 0 when its below .1
 		if (yAxisAcceleration <= 0.1) {
 			yAxisAcceleration = 0;
 		}
 	}
+	//since velocity gets added every time we move, we need to cap it
+	//velocity here can be positive or negative which that determines the direction
+	//we use abs so we don't need two separate if statements to do the same thing
 	if (std::abs(yAxisVelocity) > MAX_VELOCITY) {
+		//using signum to account for the abs
 		yAxisVelocity = Util::signum(yAxisVelocity) * MAX_VELOCITY;
 	}
+	//this brings the velocity to 0 so we don't keep moving
+	//using abs for the same reason as above
 	if (std::abs(yAxisVelocity) > 0) {
+		//using signum to account for the abs
 		yAxisVelocity -= Util::signum(yAxisVelocity) * .1;
+		//due to floating point errors we need to round down to 0 when its below .1
 		if (std::abs(yAxisVelocity) <= 0.1) {
 			yAxisVelocity = 0;
 		}
 	}
+	//set the position to add the velocity
 	pos.setY(pos.getY() + yAxisVelocity);
-	std::cout << yAxisAcceleration << "\n" << yAxisVelocity << "\n\n";
+
+	//since acceleration gets added every time we move, we need to cap it
+	if (xAxisAcceleration > MAX_ACCELERATION) {
+		xAxisAcceleration = MAX_ACCELERATION;
+	}
+	//this lowers the acceleration so we don't keep moving
+	//>= 0.1 so we can actually lower the acceleration when we are supposed to
+	if (xAxisAcceleration >= 0.1) {
+		xAxisAcceleration -= .1;
+		//due to floating point errors we need to round down to 0 when its below .1
+		if (xAxisAcceleration <= 0.1) {
+			xAxisAcceleration = 0;
+		}
+	}
+	//since velocity gets added every time we move, we need to cap it
+	//velocity here can be positive or negative which that determines the direction
+	//we use abs so we don't need two separate if statements to do the same thing
+	if (std::abs(xAxisVelocity) > MAX_VELOCITY) {
+		//using signum to account for the abs
+		xAxisVelocity = Util::signum(xAxisVelocity) * MAX_VELOCITY;
+	}
+	//this brings the velocity to 0 so we don't keep moving
+	//using abs for the same reason as above
+	if (std::abs(xAxisVelocity) > 0) {
+		//using signum to account for the abs
+		xAxisVelocity -= Util::signum(xAxisVelocity) * .1;
+		//due to floating point errors we need to round down to 0 when its below .1
+		if (std::abs(xAxisVelocity) <= 0.1) {
+			xAxisVelocity = 0;
+		}
+	}
+	//set the position to add the velocity
+	pos.setX(pos.getX() + xAxisVelocity);
+	
 }
 
 Gdiplus::Color Player::getColor() const {
--- src/game/Player.h
+++ src/game/Player.h
@@ -3,27 +3,28 @@
 #include "Pos.h"
 #include "../Paintable.h"
 #include "../Util.h"
-#include "../Direction.h"
 class Player : public Paintable{
 private:
 	//holds the alpha and rgb for the player color
 	//the way it works is by generating a random RGB value which is between 0 and 16777216 (FFFFFF in hex)
 	//then it uses "| 0xFF000000" to set the alpha of it to max because argb holds alpha first
 	int argb = Util::random(0, 0xFFFFFF) | 0xFF000000;
-	float xAxisAcceleration = 0, xAxisvelocity = 0, yAxisAcceleration = 0, yAxisVelocity = 0;
+	//used for movement in both axes respectively
+	float xAxisAcceleration = 0, xAxisVelocity = 0, yAxisAcceleration = 0, yAxisVelocity = 0;
+	//used for capping out acceleration and velocity so we don't go too fast
 	const float MAX_ACCELERATION = 2, MAX_VELOCITY = 5;
-	Direction direction = Direction::UP;
 public:
 	const static int SIDE_LENGTH = 30;
 	Pos pos;
 
 	Player();
+	//these 4 are used for moving the character in the desired direction
 	void moveUp();
 	void moveDown();
 	void moveLeft();
 	void moveRight();
-	void move();
 	void idle();
+	//overriden from Paintable.h
 	Gdiplus::Color getColor() const override;
 };
 
