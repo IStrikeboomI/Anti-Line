5de33be879627b708037c9fb6d33050ef1f4a15f
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2022-06-18 22:48:58 -0500

    Path hits end now with randomnesss, gonna fix the code later

--- src/game/PredeterminedPath.cpp
+++ src/game/PredeterminedPath.cpp
@@ -11,8 +11,12 @@ PredeterminedPath::PredeterminedPath(Pos start, Pos end) : start(start), end(end
 	const int X_START = start.getX() + start.getWidth();
 	const int Y_START = start.getY() + start.getHeight() / 2;
 
+	const float X_DISTANCE_TO_END = std::abs(end.getX() + end.getWidth() / 2 - X_START);
+	const float Y_DISTANCE_TO_END = std::abs(end.getY() + end.getHeight() / 2 - Y_START);
+	const float DISTANCE_TO_END = std::sqrt(X_DISTANCE_TO_END* X_DISTANCE_TO_END + Y_DISTANCE_TO_END* Y_DISTANCE_TO_END);
+
 	//angle from the end of the first line to the end point's middle
-	const float ANGLE_TO_END = std::atan(std::abs((float)(end.getY() + end.getHeight()/2 - Y_START) / std::abs(end.getX() + end.getWidth() / 2 - X_START)));
+	const float ANGLE_TO_END = std::atan(Y_DISTANCE_TO_END / X_DISTANCE_TO_END);
 
 	//fill out the other side lengths of the triangle with an initial longer first line
 	const float TRIANGLE_ADJACENT_LENGTH = LINE_LENGTH * std::cos(ANGLE_TO_END);
@@ -29,8 +33,33 @@ PredeterminedPath::PredeterminedPath(Pos start, Pos end) : start(start), end(end
 		//the ending of the previous line
 		Pos previousLineEnd = lines.at(lines.size() - 1)->endPos;
 
-		lines.push_back(std::make_shared<Line>(previousLineEnd.getX(), previousLineEnd.getY(), previousLineEnd.getX() + TRIANGLE_ADJACENT_LENGTH, previousLineEnd.getY() + TRIANGLE_OPPOSITE_LENGTH * SIGN));
+		//basically the same thing as above but we do it each time to account for the randomness
+		float x_distance_to_end = std::abs(end.getX() + end.getWidth() / 2 - previousLineEnd.getX());
+		float y_distance_to_end = std::abs(end.getY() + end.getHeight() / 2 - previousLineEnd.getY());
+		//angle from the end of the first line to the end point's middle
+		float angle_to_end = std::atan(y_distance_to_end / x_distance_to_end);
 
+		//fill out the other side lengths of the triangle with an initial longer first line
+		float triangle_adjacent_length = LINE_LENGTH * std::cos(angle_to_end);
+		float triangle_opposite_length = LINE_LENGTH * std::sin(angle_to_end);
+
+		float xDistance = std::abs(end.getX() + end.getWidth() / 2 - previousLineEnd.getX());
+		float yDistance = std::abs(end.getY() + end.getHeight() / 2 - previousLineEnd.getY());
+		float distance = std::sqrt(xDistance * xDistance + yDistance * yDistance);
+
+		float maxRandomDistance = (distance / DISTANCE_TO_END) * 25;
+		std::cout << maxRandomDistance << "\n" << distance << "\n" << DISTANCE_TO_END << "\n\n";
+
+		float randomiziedLength = Util::random(0,maxRandomDistance);
+
+		int sign = previousLineEnd.getY() < end.getY() ? 1 : -1;
+
+		lines.push_back(std::make_shared<Line>(previousLineEnd.getX(), previousLineEnd.getY(), previousLineEnd.getX() + triangle_adjacent_length, previousLineEnd.getY() + triangle_opposite_length * sign + randomiziedLength* sign));
+
+		//in case the line somehow misses, just break early before any memory overflow
+		if (lines.size() >= DISTANCE_TO_END/LINE_LENGTH) {
+			break;
+		}
 	}
 }
 
