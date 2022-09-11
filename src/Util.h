8000fa9cdabca199094e75b73f2fd424f354a493
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2022-09-11 15:20:10 -0500

    Made util funcitons inline

--- src/Util.h
+++ src/Util.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <random>
 namespace Util {
-	static int random(int min, int max) {
+	static inline int random(int min, int max) {
         std::random_device dev;
         std::mt19937 rng(dev());
         // distribution in range [min, max]
@@ -9,12 +9,12 @@ namespace Util {
 
         return distribution(rng);
 	}
-    static int signum(double x) {
+    static inline int signum(double x) {
         if (x > 0) return 1;
         if (x < 0) return -1;
         return 0;
     }
-    static double degToRad(double deg) {
+    static inline double degToRad(double deg) {
         //0.0174532925199432957 is pi/180
         return deg * 0.0174532925199432957;
     }
